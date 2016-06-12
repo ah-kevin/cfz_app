1e1d7004bd32d71c39e32bb8dc9aaae9d3b98d57
dg_lennon <kbjwdh@gmail.com>
2016-06-12 18:02:24 +0800

    添加login的状态

--- src/constants/login.js
+++ src/constants/login.js
@@ -1,3 +1,3 @@
-export const LOGGED_IN='LOGGED_IN';
-export const LOGGED_DOING='LOGGED_DOING';
-export const LOGGED_ERROR='LOGGED_ERROR';
+export const LOGGED_IN = 'LOGGED_IN';
+export const LOGGED_DOING = 'LOGGED_DOING';
+export const LOGGED_ERROR = 'LOGGED_ERROR';
