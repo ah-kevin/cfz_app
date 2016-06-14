0fe6237e79730e7606fb11d156fed4801a9b0788
dg_lennon <kbjwdh@gmail.com>
2016-06-14 12:45:01 +0800

    picker组件完成

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,14 +9,27 @@ import {
   View,
   Text,
   TouchableOpacity,
-  Picker
+  Dimensions
 } from 'react-native';
 import {connect} from 'react-redux';
 import Login from '../login/LoginScreen';
 import {bindActionCreators} from 'redux';
 import actions from '../actions';
-const Item = Picker.Item
+import Picker from 'react-native-picker';
+
 class Home extends Component {
+  constructor () {
+    super();
+    this.state = {
+      pickerData: [ 1, 2, 3 ],
+      selectedValue: [ 1123123123 ]
+    };
+  }
+
+  _onPressHandle () {
+    this.picker.toggle();
+  }
+
   render () {
     const { user, getLogin }=this.props;
     if (!user.isLoggedIn) {
@@ -27,7 +40,9 @@ class Home extends Component {
         <View style={styles.header}>
           <View style={styles.item}>
             <View style={[ styles.center, styles.flex ]}>
-              <Text>1</Text>
+              <TouchableOpacity onPress={this._onPressHandle.bind(this)}>
+                <Text>{this.state.selectedValue}</Text>
+              </TouchableOpacity>
             </View>
           </View>
           <View style={styles.item}>
@@ -41,17 +56,20 @@ class Home extends Component {
             </View>
           </View>
         </View>
-            {
-              false ? <View style={styles.pickerContainer}>
-                <Picker
-                  style={styles.picker}
-                  selectedValue={'111'}
-                  onValueChange={()=> {}}>
-                  <Item label="hello" value="key0" style={styles.select}/>
-                  <Item label="world" value="key1" style={styles.select}/>
-                </Picker>
-              </View> : null
-            }
+        <View style={styles.pickerContainer}>
+          <Picker
+            ref={picker => this.picker = picker}
+            style={{ height: 320 }}
+            showDuration={300}
+            pickerData={this.state.pickerData}
+            selectedValue={this.state.selectedValue}
+            onPickerDone={(pickedValue) => {
+              this.setState({
+                selectedValue: pickedValue,
+              });
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -62,9 +80,6 @@ const styles = StyleSheet.create({
     marginTop: 64,
     flex: 1,
     backgroundColor: '#fff'
-  },
-  gray: {
-    backgroundColor: 'rgba(52,52,52,.2)',
   },
   header: {
     flexDirection: 'row',
@@ -82,15 +97,8 @@ const styles = StyleSheet.create({
     flex: 1
   },
   pickerContainer: {
-    flexDirection: 'column',
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-  select: {
-    alignItems: 'flex-end'
-  },
-  picker: {
-    backgroundColor: "#fff"
+    height: Dimensions.get('window').height,
+    backgroundColor: 'red'
   }
 });
 
